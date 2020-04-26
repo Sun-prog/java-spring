2abc48a77e5a99179b6268c081846143802c9b75
Tatiana Zh <gruppa15@mail.ru>
2020-04-26 17:07:32 +0300

    Lesson 12

--- src/main/java/ru/MusicPlayer.java
+++ src/main/java/ru/MusicPlayer.java
@@ -2,6 +2,7 @@ package ru;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,6 +16,19 @@ public class MusicPlayer {
     private Music music2;
     List<String> playList;
     enum Type { CLASSIC, ROCK }
+    @Value("${musicPlayer.name}")
+    private String name;
+
+    @Value("${musicPlayer.volume}")
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
 
     @Autowired
     public MusicPlayer(@Qualifier("classicalMusic") Music music1,
